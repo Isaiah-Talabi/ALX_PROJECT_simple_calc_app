c04a3e6cf02b839eb8497b6f0fb079f394660bc0
Isaiah-Talabi <isaiahtalabi9@gmail.com>
2023-09-13 11:32:08 +0100

    ijrefi

--- main.js
+++ main.js
@@ -1,15 +1,27 @@
-const button = document.querySelectorAll('button');
- const input = document.querySelector('input');
-for(let i = 0; i < button.length; i++){
-  button[i].addEventListener('click', function(e){
-    if(button[i].innerText == '='){
-      const result = eval(input.value)
-      input.value = result;
-    } else {
-      input.value += button[i].innerHTML;
-      if(button[i].innerText == 'C'){
-        input.value = "";
-      }
-    }
-  })
+function display(val){
+
+    document.getElementById('result').value += val
+
+    return val
+
 }
+
+function solve(){
+
+    let x = document.getElementById('result').value
+
+    let y = eval(x);
+
+    document.getElementById('result').value = y
+
+    return y
+
+}
+
+function clearScreen(){
+
+    document.getElementById('result').value = ''
+
+}
+
+
